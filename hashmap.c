2fd8383a63d56133e87368cd2a5ad4c0680ca742
Your Name <you@example.com>
2026-04-17 18:51:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,7 +122,7 @@ void eraseMap(HashMap * map,  char * key)
 {    
     long pos = hash(key,map->capacity); // posicion hash original
     long i = (pos + 1) % map->capacity; // variable para recorrer hasta llegar al punto de partida
-    while(map->buckets[pios] != NULL) // mientras haya elementos
+    while(map->buckets[i] != NULL) // mientras haya elementos
         {
             printf("%s\n", map->buckets[i]->key);
             if(strcmp(map->buckets[i]->key,key) == 0) // si tienen misma llave
